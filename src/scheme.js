77f14d9dc36160ab0952a2327e4b3a113eb72a25
Daniel Nalborczyk <dnalborczyk@gmail.com>
2019-06-12 13:55:57 -0400

    Implement scheme options

--- src/scheme.js
+++ src/scheme.js
@@ -1,1 +1,23 @@
-export default function scheme() {}
+export default function scheme(server, options) {
+  const {
+    // if not set in plugin options, use env if provided
+    authToken = process.env.TWILIO_AUTH_TOKEN,
+    url,
+  } = options
+
+  if (!authToken) {
+    throw new Error(
+      'Twilio "authToken" is required for webhook request validation.',
+    )
+  }
+
+  if (!url) {
+    throw new Error('Twilio "url" is required for webhook request validation.')
+  }
+
+  return {
+    async authenticate() {
+      return true
+    },
+  }
+}
